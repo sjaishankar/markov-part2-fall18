61f593e6384d869111c21d134279632111374cf4
Sachin Jaishankar <sachin.jaishankar@duke.edu>
2018-10-19 14:00:34 -0400

    efficientwordmarkov

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -21,6 +21,33 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 	}
 	
 	public void setTraining(String text) {
-		
+		myWords = text.split("\\s+");
+		myMap = new HashMap<>();
+		for(int i = 0; i <= text.length() - myOrder; i++) {
+			WordGram wg = new WordGram(myWords, i, i + myOrder);
+			String str = text.substring(i, i+myOrder);
+			ArrayList<String> temp = new ArrayList<String>();
+			if(!myMap.containsKey(wg)) {
+				if(wg.length() + i >= text.length()) {
+					temp.add(PSEUDO_EOS);
+					myMap.put(wg, temp);
+					break;
+				}
+				temp.add(text.substring(i+wg.length(), i + wg.length() + 1));
+				myMap.put(wg, temp);
+			}
+			
+			else {
+				temp = myMap.get(wg);
+				if(wg.length() + i >= text.length()) {
+					temp.add(PSEUDO_EOS);
+					myMap.put(wg, temp);
+					break;
+				}
+				temp.add(text.substring(i+wg.length(), i + wg.length() + 1));
+				myMap.put(wg, temp);
+			}
+			
+		}
 	}
 }
